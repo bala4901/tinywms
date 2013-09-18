e4713f5603ca7d0f1f09d9eb83b7acb9a49e394c
markus <markus@markus-PC>
2013-09-18 09:30:34 +0800

    innitialize

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -6,7 +6,7 @@ class ApiController extends Controller
     /**
      * Key which has to be in HTTP USERNAME and PASSWORD headers 
      */
-    Const APPLICATION_ID = 'ASCCPE';
+    Const APPLICATION_ID = 'ASCCPE2222';
  
     /**
      * Default response format
