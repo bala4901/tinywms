8e2011c0438e4b14c8dc4e860c016db687da9c5b
bala4901 <markusbala@gmail.com>
2013-09-22 09:11:03 +0800

    Innitial Success Load desktop

--- js/App.js
+++ js/App.js
@@ -9,46 +9,42 @@ Ext.define('MyDesktop.App', {
     extend: 'Ext.ux.desktop.App',
 
     requires: [
-       // 
         'Ext.window.MessageBox',
 
         'Ext.ux.desktop.ShortcutModel',
-        'MyDesktop.core.TabWindow',
-       // 'MyDesktop.core.Notepad',
-/*
-        'MyDesktop.core.SystemStatus',
-        'MyDesktop.core.VideoWindow',
-        'MyDesktop.core.GridWindow',
-        'MyDesktop.core.TabWindow',
-        'MyDesktop.core.AccordionWindow',
-        'MyDesktop.core.Notepad',
-        'MyDesktop.core.BogusMenuModule',
-        'MyDesktop.core.BogusModule',
+
+        'MyDesktop.SystemStatus',
+        'MyDesktop.VideoWindow',
+        'MyDesktop.GridWindow',
+        'MyDesktop.TabWindow',
+        'MyDesktop.AccordionWindow',
+        'MyDesktop.Notepad',
+        'MyDesktop.BogusMenuModule',
+        'MyDesktop.BogusModule',
 
 //        'MyDesktop.Blockalanche',
-        'MyDesktop.core.Settings'*/
+        'MyDesktop.Settings'
     ],
 
-  init: function() {
+    init: function() {
         // custom logic before getXYZ methods get called...
 
         this.callParent();
 
         // now ready...
     },
-  
+
     getModules : function(){
         return [
-           new MyDesktop.TabWindow(),
-           /* new MyDesktop.VideoWindow(),
+            new MyDesktop.VideoWindow(),
             //new MyDesktop.Blockalanche(),
             new MyDesktop.SystemStatus(),
             new MyDesktop.GridWindow(),
             new MyDesktop.TabWindow(),
             new MyDesktop.AccordionWindow(),
-          new MyDesktop.Notepad(),
+            new MyDesktop.Notepad(),
             new MyDesktop.BogusMenuModule(),
-            new MyDesktop.BogusModule()*/
+            new MyDesktop.BogusModule()
         ];
     },
 
